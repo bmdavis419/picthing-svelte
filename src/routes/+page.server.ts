4fb3c56c7825e6cfd9f1831dcb5b5192a1013978
Davis <davis.benjamin41902@gmail.com>
2025-02-12 16:13:26 -0800

    streaming fix

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,5 +1,5 @@
 export const load = async () => {
-	const fakeSlowResult = await new Promise<string>((resolve) => {
+	const fakeSlowResult = new Promise<string>((resolve) => {
 		setTimeout(() => {
 			resolve('Hello from the server');
 		}, 4000);
